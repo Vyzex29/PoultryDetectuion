934d3321e8253de0201d06d22b73dab04a530379
Vyzex29 <virtuoso292@gmail.com>
2023-02-03 15:55:45 +0200

    added stream frame capture code

--- ImageScrubbing.py
+++ ImageScrubbing.py
@@ -1,7 +1,7 @@
 from simple_image_download.simple_image_download import simple_image_download
 
 response = simple_image_download
-keyword = ["chicken on field"]
+keyword = ["turkey free range"]
 
 for kw in keyword:
-    response().download(kw, 50)
+    response().download(kw, 30)
