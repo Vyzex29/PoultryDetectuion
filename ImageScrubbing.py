57ea3432391939ad89c5eea63249bef8f30ea146
Vyzex29 <virtuoso292@gmail.com>
2023-05-29 14:16:12 +0300

    Remove unnecessary files

--- ImageScrubbing.py
+++
@@ -1,7 +0,0 @@
-from simple_image_download.simple_image_download import simple_image_download
-
-response = simple_image_download
-keyword = ["turkey free range"]
-
-for kw in keyword:
-    response().download(kw, 30)
